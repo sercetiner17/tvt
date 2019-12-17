2d6adb5b92a403e305a3406d9c440472778716f6
tvtizle <58938829+tvtizle@users.noreply.github.com>
2019-12-17 21:14:54 +0300

    Add files via upload

---
+++ tvtful.php
@@ -0,0 +1,202 @@
+﻿#EXTM3U
+##EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER UHD" tvg-logo="http://kde.ipne.be/logoslar/bein-sports-haber-qa-tr.png" group-title="TR SPOR",TR: BEIN SPORTS HABER UHD
+http://86a.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/18057.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER HD" tvg-logo="http://kde.ipne.be/logoslar/bein-sports-haber-qa-tr.png" group-title="TR SPOR",TR: BEIN SPORTS HABER HD
+https://kralphp.com:443/yayin/146.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR: BEIN SPORTS 1 HD
+http://yayin10.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR: BEIN SPORTS 1 UHD
+http://maxu.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/37268.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 2 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" group-title="TR SPOR",TR: BEIN SPORTS 2 HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/bein2222/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 3 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports3hd.png" group-title="TR SPOR",TR: BEIN SPORTS 3 HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/bein3333/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 4 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports4hd.png" group-title="TR SPOR",TR: BEIN SPORTS 4 HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/bein4444/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS MAX 1 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1max.png" group-title="TR SPOR",TR: BEIN SPORTS MAX 1 UHD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax1111/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS MAX 2 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hdmax.png" group-title="TR SPOR",TR: BEIN SPORTS MAX 2 UHD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax2222/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: TIVIBUSPOR 1 UHD" tvg-logo="http://kde.ipne.be/logoslar/tivibusporhd.png" group-title="TR SPOR",TR: TIVIBUSPOR 1 UHD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu11/chunks.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: TIVIBUSPOR 2 UHD" tvg-logo="http://kde.ipne.be/logoslar/tivibuspor2hd.png" group-title="TR SPOR",TR: TIVIBUSPOR 2 UHD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu22/chunks.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: TIVIBUSPOR 3 UHD" tvg-logo="http://kde.ipne.be/logoslar/tivibuspor3.png" group-title="TR SPOR",TR: TIVIBUSPOR 3 UHD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu33/chunks.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: TIVIBUSPOR 4 UHD" tvg-logo="http://kde.ipne.be/logoslar/tivibuspor4hd.png" group-title="TR SPOR",TR: TIVIBUSPOR 4 UHD
+#EXTINF:-1 tvg-id="" tvg-name="TR: S SPOR HD" tvg-logo="" group-title="TR S SPOR HD
+https://www.underoverbet10.com:443/live/12_.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: SMART SPOR HD" tvg-logo="" group-title="TR SMART SPOR HD
+http://86a.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/89334.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: EUROSPORT1 HD" tvg-logo="" group-title="TR EUROSPORT1 HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport11/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: EUROSPORT2 HD" tvg-logo="" group-title="TR EUROSPORT2 HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport22/playlist.m3u8
+
+
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN MOVİES ACTİON HD" tvg-logo="" group-title="TR SİNEMA",TR:  BEİN MOVİES ACTİON HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN MOVİES ACTİON 2 HD" tvg-logo="" group-title="TR SİNEMA",TR:  BEİN MOVİES ACTİON 2 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction2/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN MOVİES PREMİER HD" tvg-logo="" group-title="TR SİNEMA",TR:  BEİN MOVİES PREMİER HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinmoviespremier/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN SERİES COMEDY HD" tvg-logo="" group-title="TR SİNEMA",TR:  BEİN SERİES COMEDY HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinseriescomedy/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN MOVİES STARS HD" tvg-logo="" group-title="TR SİNEMA",TR:  BEİN MOVİES STARS HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesstars/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  ANİMASYON HD" tvg-logo="" group-title="TR SİNEMA",TR:   ANİMASYON HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/animasyon/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN MOVİES TÜRK HD" tvg-logo="" group-title="TR SİNEMA",TR:   BEİN MOVİES TÜRK HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesturk/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: KORKU HD" tvg-logo="" group-title="TR SİNEMA",TR:   KORKU HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/korku/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  BEİN SERİES VİCE HD" tvg-logo="" group-title"TR SİNEMA",TR:   BEİN SERİES VİCE HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinseriesvice/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  AKSİYON HD" tvg-logo="" group-title="TR SİNEMA",TR:   AKSİYON HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/aksiyon/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BİLİM KURGU HD" tvg-logo="" group-title="TR SİNEMA",TR:   BİLİM KURGU HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/bilimkurgu/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVİES SMART ACTİON HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVİES SMART ACTİON HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/msaction/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVİES SMART FAMİLY HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVİES SMART FAMİLY HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/msfamily/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVİES SMART TURK HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVİES SMART TURK HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/msturk/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVlES SMART PLATİN HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVlES SMART PLATİN HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/msplatin/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVlES SMART PLATİN 2 HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVlES SMART PLATİN 2 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/msplatin2/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVlES SMART PREMİUM HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVlES SMART PREMİUM HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/mspremium/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: MOVlES SMART PREMİUM 2 HD" tvg-logo="" group-title="TR SİNEMA",TR:   MOVlES SMART PREMİUM 2 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/mspremium2/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEİN BOX OFFİCE 1 HD" tvg-logo="" group-title="TR SİNEMA",TR:   BEİN BOX OFFİCE 1 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinbox1/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  BEİN BOX OFFİCE 2 HD" tvg-logo="" group-title="TR SİNEMA",TR:   BEİN BOX OFFİCE 2 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinbox2/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  BEİN BOX OFFİCE 3 HD UHD" tvg-logo="" group-title="TR SİNEMA",TR:   BEİN BOX OFFİCE 3 HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/beinbox3/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: İMDB HD" tvg-logo="" group-title="TR SİNEMA",TR:   İMDB HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportssinema/imdb/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  NATIONEL GEOGRAPHİC HD" tvg-logo="" group-title="TR BELGESEL",TR:   NATIONEL GEOGRAPHİC HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/nationalgeographic/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: NAT GEO PEOPLE HD" tvg-logo="" group-title="TR BELGESEL",TR:   NAT GEO PEOPLE HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeopeople/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: NAT GEO WİLD HD" tvg-logo="" group-title="TR BELGESEL",TR:   NAT GEO WİLD HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeowild/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: DİSCOVERY CANNEL HD" tvg-logo="" group-title="TR BELGESEL",TR:   DİSCOVERY CANNEL HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverychannel/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: DİSCOVERY SEİNCE" tvg-logo="" group-title="TR BELGESEL",TR:   DİSCOVERY SEİNCE
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverysceince/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  HİSTORY CANNEL HD" tvg-logo="" group-title="TR BELGESEL",TR:   HİSTORY CANNEL HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/history/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  BBC EART HD" tvg-logo="" group-title="TR BELGESEL",TR:   BBC EART HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/bbcearth/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="TR:  ANİMAL PLANET HD" tvg-logo="" group-title="TR BELGESEL",TR:  ANİMAL PLANET HD
+http://yayin20.bunusanasoracagim.best:80/selcuksportsbelgesel/animalplanet/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR:  DAVİNCİ HD" tvg-logo="" group-title="TR BELGESEL",TR:   DAVİNCİ HD
+http://yayin10.bunusanasoracagim.best:80/selcuksportscizgifilm/davinci/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="XX:  BLUEHUSTLER" tvg-logo="" group-title="XX ADULT",XX: BLUEHUSTLER
+http://178.124.183.5:80/hls/CH_BLUEHUSTLER/variant.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="XX:  PAPATYA" tvg-logo="" group-title="XX ADULT",XX: PAPATYA
+http://178.124.183.15:80/hls/F7UMYQMCGB/bw2000000/playlist.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX: MILF" tvg-logo="" group-title="XX ADULT",XX: MILF
+
+http://live.redtraffic.xyz/milf.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX: Big Dick
+" tvg-logo="" group-title="XX ADULT",XX: Big Dick
+
+http://live.redtraffic.xyz/bigdick.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Bigtits
+" tvg-logo="" group-title="XX ADULT",XX: Bigtits
+
+http://live.redtraffic.xyz/bigtits.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX: Fetish" tvg-logo="" group-title="XX ADULT",XX: Fetish
+
+http://live.redtraffic.xyz/fetish.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Pornstar" tvg-logo="" group-title="XX ADULT",XX: Pornstar
+
+http://live.redtraffic.xyz/pornstar.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:   Big Ass" tvg-logo="" group-title="XX ADULT",XX: Big Ass
+
+http://live.redtraffic.xyz/bigass.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Interracial" tvg-logo="" group-title="XX ADULT",XX: Interracial
+
+http://live.redtraffic.xyz/interracial.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Latina
+" tvg-logo="" group-title="XX ADULT",XX: Latina
+
+http://live.redtraffic.xyz/latina.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX: POV" tvg-logo="" group-title="XX ADULT",XX: POV
+
+http://live.redtraffic.xyz/pov.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Blowjob" tvg-logo="" group-title="XX ADULT",XX: Blowjob
+
+http://live.redtraffic.xyz/blowjob.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Teen
+" tvg-logo="" group-title="XX ADULT",XX: Teen
+
+http://live.redtraffic.xyz/teen.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Hardcore" tvg-logo="" group-title="XX ADULT",XX: Hardcore
+
+http://live.redtraffic.xyz/hardcore.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Cuckold
+" tvg-logo="" group-title="XX ADULT",XX: Cuckold
+
+http://live.redtraffic.xyz/cuckold.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Threesome" tvg-logo="" group-title="XX ADULT",XX: Threesome
+
+http://live.redtraffic.xyz/threesome.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Russian
+" tvg-logo="" group-title="XX ADULT",XX: Russian
+
+http://live.redtraffic.xyz/russian.m3u8
+
+#EXTINF:-1 tvg-id="" tvg-name="XX:  Lesbian" tvg-logo="" group-title="XX ADULT",XX: Lesbian
+
+http://live.redtraffic.xyz/lesbian.m3u8
+
